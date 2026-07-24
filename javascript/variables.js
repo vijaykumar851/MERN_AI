354827d7a71cb2d0ec11f9489e28ca723a482ded
vijaykumar851 <majetivijaykumar2005@gmail.com>
2026-07-24 23:07:32 +0530

    javascript files

---
+++ javascript/variables.js
@@ -0,0 +1,30 @@
+let rollNo = "2211CS030105";
+let studentName = {
+    firstName : "Vijay",
+    lastName : "Kumar",
+}
+const fatherName = "Lakshman";
+const dateOfBirth = "13-04-2005";
+let mobileNumber = 1234512345;
+let mailId = "vijay@gmail.com";
+let password = 12345;
+const gender = "Male";
+const department = "CSE";
+const course = "Data Science";
+let image = "image.jpg";
+let city = "Hyderabad";
+let address = "142, xyz colont, hyd";
+
+console.log(rollNo);
+console.log(studentName.firstName + " " + studentName.lastName);
+console.log(fatherName);
+console.log(dateOfBirth);
+console.log(mobileNumber);
+console.log(mailId);
+console.log(password);
+console.log(gender);
+console.log(department);
+console.log(course);
+console.log(image);
+console.log(city);
+console.log(address);
