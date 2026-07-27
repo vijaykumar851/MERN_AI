f7b49c671724417a2afef47c3d6296fffb20fbf0
vijaykumar851 <majetivijaykumar2005@gmail.com>
2026-07-27 12:01:26 +0530

    adding arthametic to calculator

---
+++ calculator/script.js
@@ -0,0 +1,17 @@
+let value1 = 10;
+let value2 = 20;
+
+let addition = value1 + value2;
+console.log(addition);
+
+let subtraction = value1 - value2;
+console.log(subtraction);
+
+let multiplication = value1 * value2;
+console.log(multiplication)
+
+let divison = value1 / value2
+console.log(divison);
+
+let percentage = value1/value2 *100;
+console.log(percentage+"%");
