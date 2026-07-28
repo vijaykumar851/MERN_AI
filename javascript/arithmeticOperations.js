36a20e302f26092cd9fb46d28ffd458add6be9ed
vijaykumar851 <majetivijaykumar2005@gmail.com>
2026-07-28 22:58:49 +0530

    arithmetic operations

---
+++ javascript/arithmeticOperations.js
@@ -0,0 +1,20 @@
+let number1 = 10;
+let number2 = 20;
+
+let addition = number1 + number2;
+console.log("addition: " + addition);
+
+let subtraction = number1 - number2;
+console.log("subtraction: " + subtraction);
+
+let multiplication = number1 * number2;
+console.log("multiplication: " + multiplication);
+
+let division = number1 / number2;
+console.log("division: " + division);
+
+let modulus = number1 % number2;
+console.log("modulus: " + modulus);
+
+let Exponentiation = number1 ** number2;
+console.log("Exponentiation: " + Exponentiation);
