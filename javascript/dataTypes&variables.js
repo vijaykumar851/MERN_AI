f58a3a8708191493844069338cb4c44cd799c01d
vijaykumar851 <majetivijaykumar2005@gmail.com>
2026-07-28 22:42:10 +0530

    datatypes

---
+++ javascript/dataTypes&variables.js
@@ -0,0 +1,17 @@
+let text1 = "hello";
+let value1 = 1;
+
+console.log(text1);
+console.log(value1);
+
+const text2 = "hi";
+const value2 = 2;
+
+console.log(text2);
+console.log(value2);
+
+var text3 = "welcome";
+var value3 = 3;
+
+console.log(text3);
+console.log(value3);
