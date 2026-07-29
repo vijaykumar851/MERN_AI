1f2d9f084ca3ec8f9d277fb30de987fb2d047ed3
vijaykumar851 <majetivijaykumar2005@gmail.com>
2026-07-29 22:47:39 +0530

    update comparison operator

---
+++ javascript/operators/comparisonOperators.js
@@ -0,0 +1,16 @@
+// the comparison operator we used to compare between two values 
+// and the output will be in true or false state
+
+let number1 = 10;
+let number2 = 20;
+
+console.log(number1 > number2); // it prints which number is higher
+
+console.log(number1 < number2);
+
+console.log(number1 != number2);
+
+console.log(number1 >= number2);
+
+console.log(number1 <= number2);
+
