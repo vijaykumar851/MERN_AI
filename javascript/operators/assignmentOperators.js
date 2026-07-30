3da74104efd22fc18f1a7f073fb6e4e826db1789
vijaykumar851 <majetivijaykumar2005@gmail.com>
2026-07-30 19:29:15 +0530

    boolean operators

---
+++ javascript/operators/assignmentOperators.js
@@ -0,0 +1,17 @@
+
+// the = is used to asign a value
+
+let messages = 100;
+let readMessages = 25;
+let unreadMessages = messages - readMessages;
+
+
+let newMessages = 10;
+messages += newMessages;
+console.log(messages);
+
+
+// like += we can use all the arthametic operators 
+//  -=
+//  *=
+//  /=
---
+++ javascript/operators/booleanLogic.js
@@ -0,0 +1,21 @@
+// && it is called  and operator if both conditions are true it prints true
+let hasKey = true;
+let knowsCode = true;
+
+let openDoor = hasKey && knowsCode; 
+console.log(openDoor); 
+
+
+// || it is called OR operator if any one conditions is true it prints true
+let isWeekend = true;
+let isHoliday = false;
+
+let canRest = isWeekend || isHoliday; 
+console.log(canRest); 
+
+
+// ! it is called NOT operator it will return the opasite value if its true it will return false
+let isRaining = false;
+
+let goOutside = !isRaining; 
+console.log(goOutside); 
